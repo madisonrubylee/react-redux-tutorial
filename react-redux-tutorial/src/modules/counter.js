c9080ef1f55593c4e8d1f667667d58087df61011
madisonrubylee <lamantciel@naver.com>
2022-03-02 23:53:07 +0900

    add root reducer

---
+++ react-redux-tutorial/src/modules/counter.js
@@ -0,0 +1,31 @@
+// 액션 타입 정의.    // 모듈 이름 / 액션 이름 
+
+const INCREASE = 'counter/INCREASE'
+const DECREASE = 'counter/DECREASE'
+
+// 액션 생성 함수
+export const increase = () => ({ type : INCREASE })
+export const decrease = () => ({ type : DECREASE })
+
+//초기 상태
+const initialState = {
+    number: 0 
+}
+
+// 리듀서 함수
+function counter(state = initialState, action) {
+    switch (action.type) {
+        case INCREASE:
+            return {
+                number: state.number + 1 
+            }
+        case DECREASE: 
+            return {
+                number: state.number - 1 
+            }
+        default:
+             return state 
+    }
+}
+
+export default counter
